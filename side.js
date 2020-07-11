54716be95c74c28e5c7e2cb7b82942f1eb21ba8f
Tarungarg98 <gargtarun13@gmail.com>
2020-07-11 22:15:49 +0530

    Major Update

---
+++ side.js
@@ -0,0 +1,23 @@
+var x = window.matchMedia("(max-width: 600px)")
+function openNav() {
+	if (x.matches){
+	  document.getElementById("mySidebar").style.width = "100vw";
+	}
+	else{
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+	}
+  }
+
+function closeNav() {
+
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  
+}
+
+function closeNavMobile() {
+	if (x.matches){
+		  document.getElementById("mySidebar").style.width = "0";
+	}
+}
